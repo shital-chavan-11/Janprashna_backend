b6b07adcb8a892a3d5b4f93252cb8f9993049fa7
shital <chavanshital3108@gmail.com>
2025-09-05 22:02:15 +0530

    supeuser added true

--- Myapp/apps.py
+++ Myapp/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 from django.db.models.signals import post_migrate
-from django.conf import settings
 from django.contrib.auth import get_user_model
 import os
 
@@ -12,13 +11,14 @@ class MyappConfig(AppConfig):
         post_migrate.connect(create_admin_user, sender=self)
 
 def create_admin_user(sender, **kwargs):
-    User = get_user_model()  # Use this for custom user model
+    User = get_user_model()
 
-    ADMIN_USERNAME = "admin"
     ADMIN_EMAIL = "shitalchavan@gmail.com"
     ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD", "defaultpassword")
 
-    if not User.objects.filter(username=ADMIN_USERNAME).exists():
-        User.objects.create_superuser(username=ADMIN_USERNAME,
-                                      email=ADMIN_EMAIL,
-                                      password=ADMIN_PASSWORD)
+    # Only create superuser if it doesn't exist
+    if not User.objects.filter(email=ADMIN_EMAIL).exists():
+        User.objects.create_superuser(
+            email=ADMIN_EMAIL,
+            password=ADMIN_PASSWORD
+        )
